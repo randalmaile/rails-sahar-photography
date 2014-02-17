cd6aeab5134ea0a4b73e22605db18e3da3924753
vagrant <vagrant@rails-dev-box>
2014-02-17 21:00:38 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saharphotography::Application.config.secret_token = '87e268d784fc65ad181266a6e7b2f5869d6a74c17e97ca60231c3d899c6021ba4a655179c7fe1835f298cfc98a27cac3153601b677dcfe112a10bbf4f60a0e26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Saharphotography::Application.config.session_store :cookie_store, key: '_saharphotography_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Saharphotography::Application.config.session_store :active_record_store

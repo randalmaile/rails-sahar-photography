2412e1b642aee935bb4396cefdd767bc0009d3e3
randalmaile <randalmaile@gmail.com>
2014-02-25 13:16:33 -0800

    changed the workorder_spec.rb in elimate 'click' and used 'visit' due to javascript driver problem

--- spec/features/workorder_spec.rb
+++ spec/features/workorder_spec.rb
@@ -1,41 +1,40 @@
 require 'spec_helper'
-Capybara.default_driver = :selenium
-feature "work orders", :js => true do 
+feature "work orders" do 
   scenario "should accept all user's data from the contact/photoshoot form" do 
-    go_to_work_order_form
+    go_to_photoshoot_request_form
     expect(page).to have_content "Request a photoshoot"
-    fill_in_work_order_form
+    fill_in_photoshoot_request_form
     expect(page).to have_content "Your order has been confirmed"
   end
 
   scenario "should appear in the admin dashboard" do
-    go_to_work_order_form
-    fill_in_work_order_form
-    click_link "Admin"
-    click_link "Dashboard"
-    page.should have_content("user@email.com")
+    go_to_photoshoot_request_form
+    expect(page).to have_content "Request a photoshoot"
+    fill_in_photoshoot_request_form
+    expect(page).to have_content "Your order has been confirmed"
+    visit "/admin/index"
+    page.should have_content("rmaile@email.com")
   end
 
   scenario "should accept all admins data added to a new workorder form" do
-    
+
     
   end
 
   scenario "should accept all admin's data added to an existing workorder" do
-    visit "/admin/index"
-    cli
+
   end
 
 
-  def go_to_work_order_form
-    visit root_path
-    click_link "Contact"
-    click_link "Photoshoot"
+  def go_to_photoshoot_request_form
+    visit "/contact/photoshoot"
+    # click_link "Contact"
+    # click_link "Photoshoot"
   end
 
-  def fill_in_work_order_form
+  def fill_in_photoshoot_request_form
    fill_in 'Name', with: 'Randal Maile'
-   fill_in 'Email', with: 'user@email.com'
+   fill_in 'Email', with: 'rmaile@email.com'
    fill_in 'Phone', with: '555-555-5555'
    fill_in 'Date', with: '1/22/2014'
    select "Wedding", from: "Jobtype"
